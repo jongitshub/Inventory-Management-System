c39a6f6d708183b792c420c4b65ad621684cc1f7
jongitshub <jonbryant93@gmail.com>
2024-11-08 09:46:13 -0500

    Add files via upload

---
+++ inventory/src/inventory/Product.java
@@ -0,0 +1,14 @@
+package inventory;
+
+public class Product {
+	
+	private int id;
+	private String name;
+	private int qty;
+
+	public Product(int id, String name, int qty) {
+		this.id = id;
+		this.name = name;
+		this.qty = qty;				
+	}
+}
